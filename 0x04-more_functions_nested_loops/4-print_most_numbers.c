3d7ff07d88d6480033c02b7d55ab8f2d01a2d560
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-09-14 10:47:39 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * print_most_numbers - print 0 - 9 apart
- * from 2 and 4 and you can
- * only use _putchar twice
+ *			from 2 and 4 and you can
+ *			only use _putchar twice
  * Return: Always 0 (Success)
 */
 
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 	{
 		if (num == 2 || num == 4)
 			continue;
-			_putchar(num + 48);
+		_putchar(num + 48);
 	}
 	_putchar('\n');
 }
