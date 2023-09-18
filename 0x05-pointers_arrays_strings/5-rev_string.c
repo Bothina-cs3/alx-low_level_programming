667b3bea9b85fa746da3bf59375e08e27b518d39
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-09-18 12:32:40 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rev_string -  reverses a string
+ *
+ * @s: string input
+ *
+*/
+
+rev_string(char *s)
+{
+	int l, i;
+	char pm;
+
+	/*find string length without null char*/
+	for (l = 0; s[]; != '\0'; ++l)
+		;
+
+	/*swap the string by looping to half the string*/
+	for (i = 0; i < l / 2; i++)
+	{
+		pm = s[i];
+		s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
+		s[l 1 - i] = pm;
+	}
+}
