a4c7aba9a7c323e8fd522a090fde2610a76dc2b3
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-09-21 18:18:28 -0700

    a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -31,46 +31,34 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		a = (n1[i - 1] - '0');
 		b = (n1[j - 1] - '0');
 		if (i > 0 && j > 0)
-		{	
 			sum = a + b + l;
-		}	
-
 		else if (i < 0 && j > 0)
-		{	
-
 			sum = b + l;
-		}	
-
 		else if (i > 0 && j < 0)
-		{
-
 			sum = a + l;
-		}
-
 		else
 			sum = l;
 
 		if (sum > 9)
 		{
-		
 			l = sum / 10;
 			sum = (sum % 10) + '0';
 		}
-
 		else
 		{
-		
 			l = 0;
 			sum = sum + '0';
 		}
+
 		r[big] = sum;
+
 		i--;
 		j--;
 		big--;
-	
+	}
+
 	if (*(r) != 0)
 		return (r);
 	else
 		return (r + 1);
-
 }
