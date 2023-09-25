eba29d3c8b12c77c32e122c1b429f40e03f91bf9
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-09-25 08:43:52 -0700

    a

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strstr - function that locates a substring.
+ * @haystack: first string
+ * @needle: second string
+ * Return: pointer to the beginning of the located substring
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	int co, i, j, 
