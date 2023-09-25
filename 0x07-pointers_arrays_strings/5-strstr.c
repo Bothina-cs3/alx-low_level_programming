6e10ccf41800b473426e843bb8189aafdab45f62
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-09-25 12:14:58 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,4 +8,23 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int co, i, j, 
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h && *n && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+
+	return ('\0');
+}
