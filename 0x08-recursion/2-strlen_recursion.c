df79340f7a25e1982dddc01b9e544895092a184c
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-09-28 07:21:42 -0700

    a

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion -  function that returns the length of a string
+ * @s: string
+ * Return: the length of a string
+*/
+
+int _strlen_recursion(char *s)
+{
+	int l = 0;
+
+	if (*s > '\0')
+	{
+		l = _strlen_recursion(s + 1) + 1;
+		l++;
+	}
+	return (l);
+}
