a6fda6b20bc91c0cf8c120170b6178c97fc1c2f6
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-02 15:04:56 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints program that prints all arguments it receives.
+ * @argc: intger
+ * @argv: array
+ * Return: 0
+*/
+
+int main(int argc, char const **argv)
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i])
+			i++;
+	}
+
+	return (0);
+}
