d115e3adcac288586a4ae28a5fe1ad2ec6ccc60a
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-04 06:52:36 -0700

    a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  *create_array - function that creates an array of chars,
+ *  and initializes it with a specific char.
+ *  @size: intger
+ *  @c: array
+ *  Return: pointer to the array, or NULL if it fails
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = malloc(size);
+
+	if (size == 0 || ptr == 0)
+		return (NULL);
+	while (szie--)
+		ptr[size] = c;
+
+	return (0);
+}
