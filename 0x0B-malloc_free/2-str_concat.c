9be45b338a600a69108a32972938712623ec3fec
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-04 09:49:08 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,9 +27,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
@@ -47,5 +47,3 @@ char *str_concat(char *s1, char *s2)
 	ptr = '\0';
 	return (ptr);
 }
-
-
