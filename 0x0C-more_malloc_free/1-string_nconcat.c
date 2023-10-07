0a30735ea188c5062acdd821069e36c286c4540c
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-07 09:29:48 -0700

    a

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
 		;
-	for (s2len = 0; s2[s2_len] != '\0'; s2_len++)
+	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
 		;
 	str = malloc(s1_len + n + 1);
 	if (str == NULL)
