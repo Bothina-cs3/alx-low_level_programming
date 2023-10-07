12ddacc0f253aa079ed912c2cc850b1cda5a8ddd
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-07 06:54:50 -0700

    a

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * *malloc_checked - function that allocates memory using malloc
+ * @b: integer
+ * Return: pointer to the allocated memory
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	if (malloc == 0)
+		*ptr = *malloc_checked(unsigned int 98);
+	return (ptr);
+}
