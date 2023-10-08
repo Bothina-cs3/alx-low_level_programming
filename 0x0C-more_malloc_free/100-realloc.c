8ec6f72d8414d6378ecb61e38ef32811960e58d9
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-08 06:11:39 -0700

    a

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory allocated with malloc:malloc(old_size)
+ * @old_size: size, in bytes, of the allocated space for ptr
+ * @new_size: size, in bytes of the new memory block
+ * Return: NULL If new_size is equal to zero, and ptr is not
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *m;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		m = malloc(new_size);
+		if (m == NULL)
+			return (NULL)
+				return (m);
+	}
+	if (new_size > old_size)
+	{
+		m = malloc(new_size);
+		if (m == NULL)
+			return (NULL);
+		for (i = 0; i < old_size && i < new_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (p);
+}
+
