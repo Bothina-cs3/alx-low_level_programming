b158f368d141923c18dbd1048e735fc51d408377
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-10 08:44:17 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog
+ * Descirbtion: new type struct dog with the following elements
+ * @name: string
+ * @age: float
+ * @owner: string
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
