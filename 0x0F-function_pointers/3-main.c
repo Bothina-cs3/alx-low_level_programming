b722898a040c9a98b3ae9b6cfa067ddc03a7c08d
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-11 12:48:56 -0700

    e

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+include "3-calc.h"
+/**
+ * main - check the code
+ * @argc: int
+ * @argv: argument
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int (*op_f)(int, int);
+	int a, b;
+
+	if (argc != 4)
+		printf("Error\n");
+	exit(98);
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	op_f = get_op_func(argv[2]);
+	if (!op_f)
+		printf("Error\n");
+	exit(99);
+
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n");
+	exit(100);
+
+	printf("%d\n", op_f(a, b));
+	return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+/**
+ * op_add - adds two int
+ * @a: firest int
+ * @b: second int
+ * Return: sum a and b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts tow int
+ * @a: firest int
+ * @b: second int
+ * Return: difference a and b
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies tow int
+ * @a: firest int
+ * @b: second int
+ * Return: product of a and b
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides tow int
+ * @a: firest int
+ * @b: second int
+ * Return: result of the division of a by b
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo tow int
+ * @a: firest int
+ * @b: second int
+ * Return: remainder of the division of a by b
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
