f461f8a9d1f35fec2047dc34a9f190d9fc05cd14
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-11 09:51:24 -0700

    function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,14 +2,14 @@
 /**
  * array_iterator - function that executes
  * function given as a parameter on each element of an array
- * @aray: string
+ * @array: string
  * @size: size of array
- * @action: pointer 
+ * @action: pointer
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *d = &array + size - 1;
+	int *d = array + size - 1;
 
 	if (array && size && action)
 		while (array <= d)
