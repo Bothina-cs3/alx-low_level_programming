a1b3ac18769e57ec4d2a33510f60b75259846572
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-19 15:24:00 -0700

    ll

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * *add_node_end -nadds a new node at the end of a list_t list.
+ * *add_node - nadds a new node at the end of a list_t list.
  * @head: head node
  * @str: string
  * Return: address of the new element, or NULL if it faile
 */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	unsigned int i, num = 0;
