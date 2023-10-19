c5c6873d3743b23f18bf6368d5f628aae1ecb646
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-19 15:53:22 -0700

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,24 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	unsigned int i, num = 0;
+	unsigned int len;
+
+	if (str == NULL)
+		return (NULL)
 
 	new_node = malloc(sizeof(list_t));
 		if (new_node == NULL)
 			return (NULL);
 		new_node->str = strdup(str);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		num++;
-		new_node->len = num;
+		if (new_node->str == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+	for (len = 0; str[len] != '\0'; len++)
+		new_node->len = len;
 		new_node->next = *head;
 		*head = new_node;
-	}
-	return (*head);
+
+	return (new_node);
 }
