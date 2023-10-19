614f0f798a859f8d129ebd55d22f907c61b8f38a
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-19 16:59:07 -0700

    add

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,14 +13,14 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[%d] %s\n", h->len, "(nil)");
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
+		}
 			h = h->next;
 			nodes++;
-		}
 	}
 		return (nodes);
 }
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * *add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer head
+ * @str: string
+ * Return: address of the new element, or NULL if it failed
+*/
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode, pm;
+
+	if (str == NULL)
+		return (NULL);
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str strdup(str);
+	if (newnode->str == NULL)
+	{
+		free(newnode)
+			return (NULL);
+	}
+
+	newnode->len = strlen(str);
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+	}
+	else
+	{
+		pm = *head;
+		while (pm->next != NULL)
+			pm = pm->next;
+		pm->next = newnode;
+	}
+	return (newnode);
+}
