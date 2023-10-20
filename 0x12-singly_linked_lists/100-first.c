8bf9999a139cc7f2c83792dbfa85eb55f60fe42d
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-19 17:45:29 -0700

    add

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+include "lists.h"
+/**
+ * _constructor - before the main function is executed.
+*/
+
+
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
