ff24a93de6e5962b63dc86aeedecd0f5ea6db051
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-23 08:43:20 -0700

    aaa

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * *add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to first node
+ * @n: value for newnode
+ * Return: pointer to newnode
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (!head || newnode)
+		return (NULL);
+	newnode->next = NULL;
+	newnode->n = n;
+	if (*head)
+		newnode->next = *head;
+	*head = newnode;
+		return (newnode);
+}
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * *add_nodeint_end - adds a new node at the end of a listint_t list.
+ * @head: pointer to heder
+ * @n: newnode
+ * Return: address of the new element, or NULL if it failed
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n);
+{
+	listint_t *newnode, *p;
+
+	newnode = malloc(sizeof(listint_t));
+	if (!head || newnode)
+		return (NULL);
+
+	newnode->next = NULL;
+	newnode->n = n;
+	if (!*head)
+		*head = newnode;
+	else
+	{
+		p = *head
+			while (p->next)
+				p = p->next;
+		p->next = newnode;
+	}
+	return (newnode);
+}
