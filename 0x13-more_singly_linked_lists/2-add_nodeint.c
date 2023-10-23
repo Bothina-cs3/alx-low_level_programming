50c07ee11824ef6ff106723cffbb3d1ea7bbc1a1
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-10-23 09:14:13 -0700

    aaa

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,9 +11,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	listint_t *newnode;
 
 	newnode = malloc(sizeof(listint_t));
-	if (!head || newnode)
+	if (!head || !newnode)
 		return (NULL);
 
+	newnode->next = NULL;
 	newnode->n = n;
 		newnode->next = *head;
 	*head = newnode;
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,8 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n);
 {
-	listint_t *newnode, *p;
+	listint_t *newnode;
+	listint_t *p;
 
 	newnode = malloc(sizeof(listint_t));
 	if (!head || !newnode)
