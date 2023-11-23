dcc7d79075bd72c2b070dc2496f730ba197fdcdf
Bothina-cs3 <bothinamohammed944@gmail.com>
2023-11-23 10:29:51 -0800

    add

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include "maim.h"
+/**
+ * binary_to_uint - that converts a binary number to an unsigned int
+ * @b: pointing to a string 
+ * Return: the converted number*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	if (!b)
+		return (0);
+	while (*b)
+	{
+		if (*b != '0' && *b != '1')
+			return (1);
+		num = num * 2 + (*b++ - 48);
+	}
+	return (num);
+}
