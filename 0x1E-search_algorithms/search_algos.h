2363d12302a0e6bc55fdd7d229abe81e240b97aa
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-05-08 00:24:03 +0300

    103-exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,6 +12,7 @@ void print_array(int *array, size_t first, size_t last);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int ex_binary_search(int *array, size_t size, int value, size_t start);
 int advanced_binary(int *array, size_t size, int value);
 
 /**
