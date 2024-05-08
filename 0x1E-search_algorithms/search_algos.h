edc0ca90f4c69275d2f388ee12e3a4d1d40b7d47
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-05-08 16:41:01 +0300

    104-advanced_binary.c

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -14,6 +14,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int ex_binary_search(int *array, size_t size, int value, size_t start);
 int advanced_binary(int *array, size_t size, int value);
+int binary_rec(int *array, size_t left, size_t right, int value);
 
 /**
  * struct listint_s - singly linked list
